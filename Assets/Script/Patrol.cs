9a1f6ea9ac042b96db8161b7cade42bc89633bbb
mhuggi <emil.e.salmi@gmail.com>
2020-09-29 12:13:13 +0300

    Added robot chase and patrol

---
+++ Assets/Script/Patrol.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : StateMachineBehaviour
+{
+    GameObject[] waypoints;
+    int wpIndex = 0;
+    GameObject me;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+     waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+     me = animator.gameObject;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Vector3 direction = waypoints[wpIndex].transform.position - me.transform.position;
+       me.transform.rotation = Quaternion.Slerp(me.transform.rotation, Quaternion.LookRotation(direction), 0.04f);
+
+       if (Vector3.Distance(me.transform.position, waypoints[wpIndex].transform.position) > 0.1f) {
+           me.transform.Translate(0, 0, 0.03f);
+       } else {
+           if (wpIndex < (waypoints.Length -1)) {
+               wpIndex++;
+           } else {
+               wpIndex = 0;
+           }
+       }
+    }
+
+}
---
+++ Assets/Script/RobotAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotAI : MonoBehaviour
+{
+    GameObject enemy;
+    Animator fsm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.Find("robot");
+        fsm = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+        Vector3 direction = enemy.transform.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+        Debug.Log(angle);
+
+
+        if (distance < 8 && angle < 50) {
+            fsm.SetBool("EnemyInSight", true);
+        } else {
+            fsm.SetBool("EnemyInSight", false);
+        }
+
+    }
+}
